70f4bb6a10254c3e32a4a56a5e011e0014866407
Tóth Kevin <tothkevin0716@gmail.com>
2023-10-22 19:17:10 +0200

    Batman class

---
+++ SzuperhosProjekt/Batman.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzuperhosProjekt
+{
+    public class Batman : ISzuperhos, IMilliardos
+    {
+        private double lelemenyesseg;
+
+        public Batman()
+        {
+            this.lelemenyesseg = 100;
+        }
+
+        public void KutyutKeszit()
+        {
+            this.lelemenyesseg += 50;
+        }
+
+        public bool LegyoziE(ISzuperhos szuperhos)
+        {
+            return szuperhos.MekkoraAzEreje() < this.lelemenyesseg;
+        }
+
+        public double MekkoraAzEreje()
+        {
+            return this.lelemenyesseg * 2;
+        }
+
+        public override string ToString()
+        {
+            return $"Batman: leleményesség: {this.lelemenyesseg}";
+        }
+    }
+}
